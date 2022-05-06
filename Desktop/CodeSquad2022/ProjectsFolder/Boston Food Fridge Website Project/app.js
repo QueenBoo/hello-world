4f944a1ff032b50bcea8a75863e751dde2815f03
QueenBoo <ferrantib12@gmail.com>
2022-05-05 22:29:41 -0400

    updates to website

--- Desktop/CodeSquad2022/ProjectsFolder/Boston Food Fridge Website Project/app.js
+++ Desktop/CodeSquad2022/ProjectsFolder/Boston Food Fridge Website Project/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const path = require('path');
+const methodOverride = require('method-override');
+const routes = require('./routes/index');
 const app = express();
-
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
-app.listen( PORT, () => {
-    console.log('Server is running on ${PORT}');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(routes);
+
+require('./config/connection');
+
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`);
 });
